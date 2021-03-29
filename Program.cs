7fdcb72b6e28f1b49d4946d1373933ae2f85a90c
spjuneman <spjuneman@gmail.com>
2021-03-29 11:02:47 -0500

    Hello World

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            //double c = Convert.ToDouble(Console.ReadLine());
+            //double d = Convert.ToDouble(Console.ReadLine());
+
+            Test<int> t1 = new Test<int>();
+            t1.Addtest(a, b);
+            Test<int>.Add(a, b);
+            Test<int>.Sub(a, b);
+            Test<int>.Mul(a, b);
+            Test<int>.Div(a, b);
+            Console.ReadLine();
+        }
+
+        public static bool Compare<T>(T a, T b)
+        {
+            if (a.Equals(b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }   
+}
---
+++ Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_2021
+{
+    public class Test <T>
+    {
+        public void Addtest(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+
+        public static void Add(T a, T b)
+        {
+            // Datatype that have to be created on the go(at run time)
+            dynamic d1 = a;
+            dynamic d2 = b;
+
+            Console.WriteLine(d1 + d2);
+        }
+        public static void Sub(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+
+            Console.WriteLine(d1 - d2);
+        }
+        public static void Mul(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+
+            Console.WriteLine(d1 * d2);
+        }
+        public static void Div(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+
+            Console.WriteLine(d1 / d2);
+        }
+    }
+}
